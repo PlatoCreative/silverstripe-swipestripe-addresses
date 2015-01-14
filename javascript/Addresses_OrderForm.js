410ab30b6f43cdcdc40661f1014916d07e343a1c
Myles Beardsmore <mediabeastnz@gmail.com>
2015-01-15 08:51:11 +1300

    Fixed minor spelling issue.

--- javascript/Addresses_OrderForm.js
+++ javascript/Addresses_OrderForm.js
@@ -10,7 +10,7 @@ jQuery(document).ready(function($){
 				this.on('change', function(e) {
 					self._copyAddress(e);
 				});
-				
+
 				$('#address-shipping input[type=text], #address-shipping select', form).on('keyup blur', function(e){
 					self._copyAddress(e);
 				});
@@ -23,7 +23,7 @@ jQuery(document).ready(function($){
 			onunmatch: function() {
 				this._super();
 			},
-			
+
 			_copyAddress: function(e) {
 				var form = this.closest('form');
 
@@ -46,9 +46,9 @@ jQuery(document).ready(function($){
 		});
 
 	});
-	
+
 });
-	
+
 jQuery(document).ready(function($){
 	// order form submission
 	$("#OrderForm_OrderForm").submit(function() {
@@ -62,26 +62,26 @@ jQuery(document).ready(function($){
 			return false;
 		}
 	});
-	
-	function refreshEventListeners(){		
+
+	function refreshEventListeners(){
 		// hide normal form but keep it on the page
 		$(".customer-addresses").hide();
-		
+
 		// manage shipping selections
 		$("#shipping .selectable").unbind('click');
-		
+
 		$("#shipping .selectable").click(function(){
 			// set session shipping variable
 			$.post('checkout/setAddressID', {'ShippingAddressID' : $(this).attr('data-id')});
-		
+
 			$("#shipping").find( ".callout" ).removeClass("callout selected");
 			$(this).parent().parent().addClass("callout selected");
-			
+
 			var addressID = $(this).data("id");
 			var typeOfAddress = "shipping";
 			var dataquery = { addressID: addressID, type: typeOfAddress };
 			modal = $(this);
-			
+
 			$.ajax({
 				type: "GET",
 				url : "account/getAddress",
@@ -103,27 +103,27 @@ jQuery(document).ready(function($){
                     //update cart
                     $('.order-form').entwine('sws').updateCart();
 				}
-			});	
+			});
 		});
-		
+
 		// manage billing selections
 		$("#billing .selectable").unbind('click');
 		$("#billing .selectable").click(function(){
 			$("#billing").find( ".callout" ).removeClass("callout");
 			$(this).parent().parent().addClass("callout");
-			
+
 			var addressID = $(this).data("id");
-			var typeOfAddress = "shipping";
+			var typeOfAddress = "billing";
 			var dataquery = { addressID: addressID, type: typeOfAddress };
 			modal = $(this);
-			
+
 			$.ajax({
 				type: "GET",
 				url : "account/getAddress",
 				data: dataquery,
 				dataType : 'json',
 				success: function(data) {
-					
+
 					$('.address #address-billing #BillingFirstName input').val(data.FirstName);
 					$('.address #address-billing #BillingSurname input').val(data.Surname);
 					$('.address #address-billing #BillingCompany input').val(data.Company);
@@ -132,24 +132,24 @@ jQuery(document).ready(function($){
 					$('.address #address-billing #BillingCity input').val(data.City);
 					$('.address #address-billing #BillingPostalCode input').val(data.PostalCode);
 					$('.address #address-billing #BillingState input').val(data.State);
-					//$('.address #address-billing #BillingCity input').val(data.City);	
-					
+					//$('.address #address-billing #BillingCity input').val(data.City);
+
 				}
-			});	
-			
-		});	
-		
+			});
+
+		});
+
 		// delete selected address
 		$(".delete-address").unbind('click');
 		$(".delete-address").click(function(){
 			var cnfm = confirm("Are you sure you want to delete this address?");
 			if (cnfm == true) {
 				btn = $(this);
-				
+
 				var addressID = $(this).data("id");
 				var typeOfAddress = $(this).data("type");
 				var dataquery = { addressID: addressID, type: typeOfAddress };
-				
+
 				$.ajax({
 					type: "POST",
 					url : "account/deleteAddress",
@@ -157,16 +157,16 @@ jQuery(document).ready(function($){
 					success : function(data){
 						// if success
 						if(data == 1){
-							
-							btn.find("span").html("DELETING").css("background-color", "red");						
+
+							btn.find("span").html("DELETING").css("background-color", "red");
 							setTimeout(function(){
 								// after a second or two remove the deleted item from the DOM
 								btn.closest( "li" ).remove();
 							}, 2000);
-							
+
 						}else{
 							// silly animatation to show user something failed
-							btn.find("span").html("DELETING").css("background-color", "red");					
+							btn.find("span").html("DELETING").css("background-color", "red");
 							setTimeout(function(){
 								btn.find("span")
 									.animate({'left':(+10)+'px'},200)
@@ -176,13 +176,13 @@ jQuery(document).ready(function($){
 									.animate({'left':(+0)+'px'},200);
 									btn.find("span").html("REMOVE").css("background-color", "");
 								}, 2000);
-								
+
 						}
 					}
 				});
 			}
 		});
-		
+
 		// get address details before edit
 		$(".edit-address").unbind('click');
 		$(".edit-address").click(function(){
@@ -190,14 +190,14 @@ jQuery(document).ready(function($){
 			var typeOfAddress = $(this).data("type");
 			var dataquery = { addressID: addressID, type: typeOfAddress };
 			modal = $(this);
-			
+
 			$.ajax({
 				type: "GET",
 				url : "account/getAddress",
 				data: dataquery,
 				dataType : 'json',
 				success: function(data) {
-					console.log(data);   
+					console.log(data);
 					if(typeOfAddress == "shipping"){
 						$('#shippingAddressModal #ShippingFirstName input').val(data.FirstName);
 						$('#shippingAddressModal #ShippingSurname input').val(data.Surname);
@@ -208,9 +208,9 @@ jQuery(document).ready(function($){
 						$('#shippingAddressModal #ShippingPostalCode input').val(data.PostalCode);
 						$('#shippingAddressModal #ShippingState input').val(data.State);
 						//$('#shippingAddressModal #ShippingCity input').val(data.City);
-						
+
 						$("#shippingAddressModal form").data("id", addressID);
-						
+
 					}else{
 						$('#billingAddressModal #BillingFirstName input').val(data.FirstName);
 						$('#billingAddressModal #BillingSurname input').val(data.Surname);
@@ -221,25 +221,25 @@ jQuery(document).ready(function($){
 						$('#billingAddressModal #BillingPostalCode input').val(data.PostalCode);
 						$('#billingAddressModal #BillingState input').val(data.State);
 						//$('#billingAddressModal #BillingCity input').val(data.City);
-						
+
 						$(this).data( $("#billingAddressModal form"), "id", addressID );
-					}						
+					}
 				}
-			});	
-		});		
+			});
+		});
 	}
 	refreshEventListeners();
-	
+
 	// edit address
 	$("#shippingAddressModal form, #billingAddressModal form").submit(function (e) {
 		e.preventDefault();
-		
+
 		var dataString = $(this).serializeArray();
 		var addressID = $(this).data("id");
 		var typeOfAddress = $(this).data("type");
-		
+
 		dataString.push({ name: "type", value: typeOfAddress });
-		dataString.push({ name: "addressID", value: addressID });		
+		dataString.push({ name: "addressID", value: addressID });
 
 		$.ajax({
 			type: "POST",
@@ -249,32 +249,32 @@ jQuery(document).ready(function($){
 			success: function(data) {
 				// make the new element
 				var newAddressContent = data.Address + '<br>' + data.City;
-				
+
 				// add new address to the DOM so no reload is required.
 				if(typeOfAddress == "shipping"){
 					$("ul#shipping").find("li .panel .selectable").html(newAddressContent);
 				}else{
 					$("ul#billing").find("li .panel .selectable").html(newAddressContent);
 				}
-				
+
 				refreshEventListeners();
-				
+
 				$('.reveal-modal').foundation('reveal', 'close');
 			}
 		});
-		
-		return false;		
-	});	
-	
+
+		return false;
+	});
+
 
 	// add address
 	$("#addShippingAddress form").submit(function (e) {
 		e.preventDefault();
-		
+
 		var dataString = $(this).serializeArray(),
 			typeOfAddress = $(this).data("type");
 		dataString.push({ name: "type", value: typeOfAddress });
-		
+
 		$.ajax({
 			type: "POST",
 			url : "account/addAddress",
@@ -282,7 +282,7 @@ jQuery(document).ready(function($){
 			dataType : 'json',
 			success: function(data) {
 				var newAddressElement = '<li><div class="panel address"><p><a href="javascript:;" data-id="' + data.ID + '" class="selectable">' + data.Address + '<br>' + data.City + '</a><br><br><a href="javascript:;" data-id="' + data.ID + '" data-reveal-id="shippingAddressModal"><span class="label success">EDIT</span></a> <a href="javascript:;" data-id="' + data.ID + '" data-type="shipping" class="delete-address"><span class="label warning">DELETE</span></a></p></div></li>';
-				
+
 				// add new address to the DOM so no reload is required.
 				if(typeOfAddress == "shipping"){
 					$("ul#shipping").prepend(newAddressElement);
@@ -290,11 +290,11 @@ jQuery(document).ready(function($){
 					$("ul#billing").prepend(newAddressElement);
 				}
 				refreshEventListeners();
-				
-				$('.reveal-modal').foundation('reveal', 'close');				
+
+				$('.reveal-modal').foundation('reveal', 'close');
 			}
 		});
-		
+
 		return false;
 	});
-});	
+});
